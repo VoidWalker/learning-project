fca39f3626f3bacbe101847a22a8ba69f4e64c11
VoidWalker <sohanoleksa@gmail.com>
2016-01-17 18:17:42 +0200

    ajaxs key transform

---
+++ ajax/ajaxResponse.php
@@ -0,0 +1,19 @@
+<?php
+$requestTransformed = "";
+if (!empty($_GET['value'])) {
+    $request = $_GET['value'];
+    $lastChar = substr($request, -1);
+    $lastCharTransformed = isUpperCase($lastChar)?strtolower($lastChar):strtoupper($lastChar);
+    $requestTransformed = $lastCharTransformed;
+
+} else {
+    $requestTransformed = "NoValue!";
+}
+
+function isUpperCase($char)
+{
+    $upper = strtoupper($char);
+    return ($upper === $char);
+}
+
+echo $requestTransformed;
