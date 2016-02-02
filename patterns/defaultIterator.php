b3db763fe7e2b62ef237016e03d251de58066c97
VoidWalker <sohanoleksa@gmail.com>
2016-02-02 22:39:21 +0200

    Patterns

---
+++ patterns/defaultIterator.php
@@ -0,0 +1,24 @@
+<?php
+class myIterator implements IteratorAggregate {
+    private $array = array(
+        "firstelement",
+        "secondelement",
+        "lastelement",
+    );
+
+    public function __construct() {
+        $this->position = 0;
+    }
+
+    public function getIterator(){
+        return new ArrayIterator($this->array);
+    }
+
+}
+
+$it = new myIterator;
+
+foreach($it as $key => $value) {
+    var_dump($key, $value);
+    echo "\n";
+}
