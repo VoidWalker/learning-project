5938976e088cc7f3dcd8f118b9e87669dde221c4
VoidWalker <sohanoleksa@gmail.com>
2016-05-18 22:53:26 +0300

    mysqli  check

--- sql_inject/sql_inject.php
+++ sql_inject/sql_inject.php
@@ -6,10 +6,12 @@ $author = $_POST['author'];
 $message = $_POST['message'];
 
 $db = DB::DBInstance()->DB();
-$stmt = $db->prepare('INSERT INTO message (id, time, author, message) VALUES (null, :time, :author, :message)');
-$stmt->execute(array(':time' => $time, ':author' => $author, ':message' => $message));
-//$author = $db->quote($author);
-//$message = $db->quote($message);
-//$db = $db->query("INSERT INTO message (id, time, author, message) VALUES (null, $time, $author, $message)");
+//$stmt = $db->prepare('INSERT INTO message (id, time, author, message) VALUES (null, :time, :author, :message)');
+//$stmt->execute(array(':time' => $time, ':author' => $author, ':message' => $message));
+$author = $db->quote($author);
+$message = $db->quote($message);
+//$author = htmlspecialchars($author);
+//$message = htmlspecialchars($message);
+$db = $db->query("INSERT INTO message (id, time, author, message) VALUES (null, '$time', '$author', '$message')");
 header("Location: http://learning-project.local/sql_inject/ajax_chat.html"); /* Redirect browser */
 exit();
