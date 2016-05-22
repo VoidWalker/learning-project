5400dcdaaed21ff5b4e5c6260e84b3dcd5b78c40
VoidWalker <sohanoleksa@gmail.com>
2016-05-22 23:21:48 +0300

    mysqli  check

--- test.php
+++ test.php
@@ -1,26 +1,8 @@
 <?php
-
-
-//procedure method
-//$db_link = mysqli_connect("localhost", "root", "", "learning_db");
-//if (!$db_link) {
-//    die('Connection error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
-//}
-//
-//echo "Connection established... " . mysqli_get_host_info($db_link) . "\n";
-//
-//mysqli_close($db_link);
-
-$mysqli = new mysqli('localhost', 'root', '', 'learning_db');
-
-if ($mysqli->connect_error) {
-    die('Connection error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
-echo "Connection established... " . $mysqli->host_info . "\n";
-
-$result = $mysqli->query("SELECT * FROM message");
-echo "<pre>";
-print_r($result->fetch_all());
-echo "</pre>";
-
-$mysqli->close();
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+session_start();
+if (!isset($_SESSION['counter'])) $_SESSION['counter'] = 0;
+echo session_id() . "Page was refreshed " . $_SESSION['counter']++ . " times. ";
+echo "<br><a href=" . $_SERVER['PHP_SELF'] . ">refresh";
+unset($_SESSION['counter']);
